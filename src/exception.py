5ee733c30b0256b2d741dccf5f99ea8fccf0f0b0
utkarshg1 <gaikwadujg@gmail.com>
2023-03-15 20:02:58 +0530

    Logger and exception updated

--- src/exception.py
+++ src/exception.py
@@ -1,10 +1,11 @@
 import sys
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
 
-    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{3}]".format(
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name, exc_tb.tb_lineno, str(error)
     )
 
@@ -13,8 +14,8 @@ def error_message_detail(error,error_detail:sys):
 class CustomException(Exception):
     
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
-        self.error_detail = error_message_detail(error_message, error_detail=error_detail)
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
     def __str__(self):
-        return self.error_message
+        return self.error_message    
