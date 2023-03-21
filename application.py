1ae7af871adfd8b0d3d23335d5fc4927ad0debfd
utkarshg1 <gaikwadujg@gmail.com>
2023-03-21 23:48:33 +0530

    Flask app skeleton completed

--- application.py
+++ application.py
@@ -8,3 +8,9 @@ application = Flask(__name__)
 
 app = application
 
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
