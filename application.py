4b06272d2e12165c39339a7e4a97fc35b5ce6641
utkarshg1 <gaikwadujg@gmail.com>
2023-03-22 09:44:41 +0530

    application.py updated

--- application.py
+++ application.py
@@ -1,11 +1,5 @@
 from flask import Flask, request, render_template
 from flask_cors import CORS,cross_origin
-
-import numpy as np
-import pandas as pd
-
-from sklearn.preprocessing import StandardScaler
-
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
