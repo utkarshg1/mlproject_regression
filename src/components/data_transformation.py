ffbd434038b9ed277e619ad7ebb89667026716a5
utkarshg1 <gaikwadujg@gmail.com>
2023-03-22 12:32:42 +0530

    Data Transformation updated

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -30,7 +30,7 @@ class DataTransformation:
             
             # Define which columns should be ordinal-encoded and which should be scaled
             categorical_cols = ['cut', 'color','clarity']
-            numerical_cols = ['carat', 'table', 'x', 'y', 'z']
+            numerical_cols = ['carat', 'depth','table', 'x', 'y', 'z']
             
             # Define the custom ranking for each ordinal variable
             cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
